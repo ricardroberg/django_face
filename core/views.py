107425cc27c058b84a275c00cadd951e619f0007
Ricard Roberg <ricardroberg@gmail.com>
2020-01-17 14:37:23 -0300

    got a flu, nothing usefull done. will continue tomorow

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('<h1>Home Page</h1>')
+
+
+def login(request):
+    return HttpResponse('<h1>Log In</h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>About</h1>')
--- django_face/settings.py
+++ django_face/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'y3-whiw)q_lcedg&1l8w46-d5bw%4kcu-9fh=iojsu8v=8+k-v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
